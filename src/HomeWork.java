b06059c3b8cb27de5d047574df2e0ff2f4b52646
Денис Макаров <denismakarov@MacBook-Pro-Denis.local>
2021-06-07 19:47:22 +0300

    fixed count from first to second salary tax

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -2,18 +2,17 @@ public class HomeWork {
 
 
     public static void main(String[] args) {
-// --> own solution
-        double tax = index(newTax(1000), 2);
-        System.out.println(tax);
+// --> own solution with recurs
+        double tax = index(newTax(500), 1);
+        System.out.println("home: " + tax);
 
 
-//--> from lesson
-//        double salary = 8000;
-//        double res = taxIsrael(salary, 2.25);
-//        System.out.println(res);
+//--> solution from lesson
+        double salary = 500;
+        double res = taxIsrael(salary, 0);
+        System.out.println("class: " + res);
 //        double salaryNetto = salaryNetto(salary, res);
 //        System.out.println(salaryNetto);
-//        System.out.println();
     }
 
     //// ------------  Solution from lesson ------------ ////
@@ -23,25 +22,26 @@ public class HomeWork {
     }
 
     public static double taxIsrael(double salary, double taxAllowance) {
-        double tax = -(taxAllowance * 219);
+//        double tax = -(taxAllowance * 219);
+        double tax = 0;
 
 
-        if (salary < 6330) {
+        if (salary < 6000) {
             double res = tax + salary * 0.1;
             return res > 0 ? res : 0;
         } else {
-            tax = tax + 6330 * 0.1;
+            tax = tax + 6000 * 0.1;
         }
 
-        if (salary >= 6330 && salary < 9080) {
-            double res = tax + (salary - 6330) * 0.14;
+        if (salary >= 6000 && salary < 9000) {
+            double res = tax + (salary - 6000) * 0.14;
             return res > 0 ? res : 0;
         } else {
-            tax = tax + (9080 - 6330) * 0.14;
+            tax = tax + (9000 - 6000) * 0.14;
         }
 
-        if (salary >= 9080 && salary < 14580) {
-            double res = tax + (salary - 9080) * 0.2;
+        if (salary >= 9000 && salary < 14580) {
+            double res = tax + (salary - 9000) * 0.2;
             return res > 0 ? res : 0;
         } else {
             tax = tax + (14580 - 9080) * 0.2;
@@ -74,30 +74,38 @@ public class HomeWork {
 
     //// ------------  Solution with recursion ------------ ////
     private static int posOfSalary = 0;
-    private static double tax = 0;
+    private static double tax;
+    private static double posOfTax = 0;
+
 
     public static double index(double tax, double ind) {
-        double res = tax - (ind * 219);
-        return (res < 0) ? 0 : res;
+//        double res = tax - (ind * 219);
+        double res = tax;
+//        return (res < 0) ? 0 : res;
+        return res;
     }
 
     public static double newTax(double salary) {
-        double[] posSal = {2000, 3000, 4000, 5000, 6000, 7000};
-        double[] posTax = {0.1, 0.1, 0.1, 0.1, 0.1, 0.2};
+        double[] posSal = {6000, 9000, 14580, 20260, 42160, 54300};
+        double[] posTax = {0.1, 0.14, 0.2, 0.31, 0.35, 0.47, 0.5};
 //        double idx = tax-(index * 1);
 
         if (posOfSalary == posSal.length - 1) {
-            tax = (salary - posSal[posOfSalary]) * posTax[posOfSalary] + tax;
+            tax = (salary - posSal[posOfSalary]) * posTax[posOfSalary + 1] + tax + (posSal[0] * posTax[0]);
 //            tax += res;
         } else if (salary >= posSal[posOfSalary] && salary >= posSal[posOfSalary + 1]) {
-            tax = (posSal[posOfSalary + 1] - posSal[posOfSalary]) * posTax[posOfSalary] + tax;
+//            tax = (posSal[0]) * posTax[posOfSalary] + tax;
+            tax = (posSal[posOfSalary + 1] - posSal[posOfSalary]) * posTax[posOfSalary + 1] + tax;
 //            tax += res;
             posOfSalary += 1;
+            posOfTax += 1;
             newTax(salary);
-        } else if (salary > posSal[posOfSalary] && salary < posSal[posOfSalary + 1]) {
-            tax = (salary - posSal[posOfSalary]) * posTax[posOfSalary] + tax;
+        } else if (salary >= posSal[posOfSalary] && salary < posSal[posOfSalary + 1]) {
+            tax = ((salary - posSal[posOfSalary]) * posTax[posOfSalary +1] + tax) + (posSal[0] * posTax[0]);
 //            System.out.println(res);
 //            tax = res;
+        } else if (salary <= posSal[0]) {
+            tax = (salary) * posTax[posOfSalary] + tax;
         }
         return tax;
     }
