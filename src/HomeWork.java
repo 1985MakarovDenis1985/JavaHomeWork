52a2dbdbda5393f8f01510e27a7bd9f5f48e6896
Денис Макаров <denismakarov@MacBook-Pro-Denis.local>
2021-06-10 09:05:15 +0300

    hw 8 completed

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,47 +1,27 @@
 public class HomeWork {
 
     public static void main(String[] args) {
-        starsFun(34, 7);
+        starsFun(14, 5);
     }
-
 
     //// ------ lesson 8 ------- ////
     public static void starsFun(int n, int stars) {
-        int x = n % stars + 1;
-        System.out.println(x);
+        int reminder = n%stars;
+        n = n + (stars-n%stars);
 
-        while (n > 0) {
-            if (n % stars != x) {
-                if (n != 0) {
-                    System.out.print("*");
-                } else if (n== 7) {
-                    System.out.println("*");
-                }
-                n--;
-            } else if(n % stars != x){
+        while (n > stars-reminder) {
+            if (n%stars == 1) {
                 System.out.println("*");
                 n--;
+            } else {
+                if (n != stars-reminder+1){
+                    System.out.print("*");
+                    n--;
+                }else {
+                    System.out.println("*");
+                    n--;
+                }
             }
         }
     }
-
-//    public static void stars(int n, int stars) {
-//        boolean check = n%stars != 0;
-//        int finger = 1;
-//        while (n > 0) {
-//            if (finger < stars ){
-//                System.out.print("*");
-//                finger++;
-//            }else{
-//                System.out.println("*");
-//                finger = 1 ;
-//            }
-//            n--;
-//        }
-//        if (check){
-//            System.out.println();
-//        }
-//    }
-
-
 }
