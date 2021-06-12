dc0e2b93412464b0c41ce3be44d2d0a78e2ff83d
Денис Макаров <denismakarov@MacBook-Pro-Denis.local>
2021-06-12 17:37:24 +0300

    add second sol

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,17 +1,18 @@
 public class HomeWork {
 
     public static void main(String[] args) {
-        boolean res = luckyTicket(1441);
+        boolean res = luckyTicket(1331);
         System.out.println("lucky: " + res);
     }
 
     //// ------ lesson 9 ------- ////
 
+// --- with for ---
     public static boolean luckyTicket(int num) {
         int odd = 0;
         int even = 0;
-        boolean digitOdd=false;
-        while (num != 0) {
+        boolean digitOdd;
+        for (digitOdd=false; num != 0; num/=10) {
             if (digitOdd==false){
                 odd += num % 10;
                 digitOdd=true;
@@ -19,10 +20,25 @@ public class HomeWork {
                 even += num % 10;
                 digitOdd=false;
             }
-            num /= 10;
         }
-        System.out.println("left: " + odd);
-        System.out.println("right: " + even);
         return odd == even ? true : false;
     }
+
+// --- with wile ---
+//    public static boolean luckyTicket(int num) {
+//        int odd = 0;
+//        int even = 0;
+//        boolean digitOdd=false;
+//        while (num != 0) {
+//            if (digitOdd==false){
+//                odd += num % 10;
+//                digitOdd=true;
+//            } else {
+//                even += num % 10;
+//                digitOdd=false;
+//            }
+//            num /= 10;
+//        }
+//        return odd == even ? true : false;
+//    }
 }
