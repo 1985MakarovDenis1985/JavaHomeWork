795afb264553459ffd0f41b1a4b7106e3d2f30a8
Денис Макаров <denismakarov@MacBook-Pro-Denis.local>
2021-06-16 20:07:54 +0300

    first path copmpleted

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,87 +1,69 @@
 public class HomeWork {
 
     public static void main(String[] args) {
-        boolean res = LN3(2310);
-        System.out.println("lucky: " + res);
+        figure1(8);
+        System.out.println();
+        figure2(8);
+        System.out.println();
+        figure3(8);
+        System.out.println();
+        figure4(8);
     }
 
-    //// ------ lesson 9 ------- ////
 
-    // --- HW --- class --- обьяснить как работает (какая идея)
-
-    public static boolean LN3(int number) {
-        int sum = 0;
-        while (number != 0) {
-            sum = number % 10 - sum;
-            number /= 10;
+    public static void figure4(int n) {
+        int a = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j >= i && j < a || i == 1 || i == n || j == 1 || j == n || i == j || j == a) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            a--;
+            System.out.println();
         }
-        return sum == 0;
     }
 
-    // ----- ------ ------
+    public static void figure3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j <= i || i == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
-//    public static boolean LN2(int number) {
-//        int sum1 = 0;
-//        int sum2 = 0;
-//        while (number != 0) {
-//            sum1 += number % 10;
-//            number /= 10;
-//            sum2 += number % 10;
-//            number /= 10;
-//        }
-//        return sum1 == sum2;
-//    }
+    public static void figure2(int n) {
+        int a = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n || i == j || j == a) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            a--;
+            System.out.println();
+        }
+    }
 
-// ----- ------ ------
-
-//    public static boolean LN1(int number) {
-//        int sum1 = 0;
-//        int sum2 = 0;
-//        int pos =1;
-//        while (number!=0){
-//            if (pos%2 == 0){
-//                sum1 += number%10;
-//            }else {
-//                sum2 += number%10;
-//            }
-//            pos++;
-//            number/=10;
-//        }
-//        return sum1==sum2;
-//    }
-
-// --- with for ---
-//    public static boolean luckyTicket(int num) {
-//        int odd = 0;
-//        int even = 0;
-//        boolean digitOdd;
-//        for (digitOdd=false; num != 0; num/=10) {
-//            if (digitOdd==false){
-//                odd += num % 10;
-//                digitOdd=true;
-//            } else {
-//                even += num % 10;
-//                digitOdd=false;
-//            }
-//        }
-//        return odd == even ? true : false;
-//    }
-
-// --- with wile ---
-//    public static boolean luckyTicket(int num) {
-//        int odd = 0;
-//        int even = 0;
-//        boolean digitOdd=false;
-//        while (num != 0) {
-//            if (digitOdd==false){
-//                odd += num % 10;
-//                digitOdd=true;
-//            } else {
-//                even += num % 10;
-//                digitOdd=false;
-//            }
-//            num /= 10;
-//        }
-//        return odd == even ? true : false;
-//    }
+    public static void figure1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
