216974486fe896dcfb8531152e6341d4c71ebc0b
Денис Макаров <denismakarov@MacBook-Pro-Denis.local>
2021-06-17 19:14:25 +0300

    HW 11: revers arr and tax Israel by arr

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,69 +1,71 @@
 public class HomeWork {
 
     public static void main(String[] args) {
-        figure1(8);
+        int[] arr = new int[10];
+        printArr(arr);
+        revArrVoid(arr);
+        System.out.println("--- old arr ---");
+        printArr(arr);
+        System.out.println("--- fun revers");
+        reversArr(arr);
+        System.out.println("--- old arr ---");
+        printArr(arr);
+
+
+        // --> TAX ISRAEL - solution with recurs and arrays
         System.out.println();
-        figure2(8);
+        System.out.println("TAX ISRAEL - solution with recurs and arrays: ");
+        double salary = 10000;
+        double indexOfTax = 2.5;
+        double tax = newTax(salary, indexOfTax);
+        System.out.println("tax: " + tax);
+    }
+
+    public static void revArrVoid(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(i * i + "\t");
+        }
         System.out.println();
-        figure3(8);
+    }
+    public static int[] reversArr(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr[i] = ((arr.length - 1) - i) * ((arr.length - 1) - i);
+        }
+        return arr;
+    }
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
         System.out.println();
-        figure4(8);
     }
 
 
-    public static void figure4(int n) {
-        int a = n;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (j >= i && j < a || i == 1 || i == n || j == 1 || j == n || i == j || j == a) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            a--;
-            System.out.println();
-        }
-    }
+    /// ------ tax Israel by arrays and recurs
 
-    public static void figure3(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (j <= i || i == 1 || j == n) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-    }
 
-    public static void figure2(int n) {
-        int a = n;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 || i == n || j == 1 || j == n || i == j || j == a) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            a--;
-            System.out.println();
-        }
-    }
+    //// ------------  Solution with recursion ------------ ////
+    private static int posOfSalary = 0;
+    private static double tax;
 
-    public static void figure1(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 || i == n || j == 1 || j == n || i == j) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
+    public static double newTax(double salary, double index) {
+        double[] posSal = {6000, 9000, 14580, 20260, 42160, 54300};
+        double[] posTax = {0.1, 0.14, 0.2, 0.31, 0.35, 0.47, 0.5};
+        double idx = -(index * 219);
+
+        if (posOfSalary == posSal.length - 1) {
+            tax = (salary - posSal[posOfSalary]) * posTax[posOfSalary + 1] + tax + (posSal[0] * posTax[0]) + idx;
+        } else if (salary >= posSal[posOfSalary] && salary >= posSal[posOfSalary + 1]) {
+            tax = (posSal[posOfSalary + 1] - posSal[posOfSalary]) * posTax[posOfSalary + 1] + tax;
+            posOfSalary += 1;
+            newTax(salary, index);
+        } else if (salary >= posSal[posOfSalary] && salary < posSal[posOfSalary + 1]) {
+            tax = ((salary - posSal[posOfSalary]) * posTax[posOfSalary + 1] + tax) + (posSal[0] * posTax[0]) + idx;
+        } else if (salary <= posSal[0]) {
+            tax = (salary) * posTax[posOfSalary] + tax + idx;
         }
+        tax = (tax <= 0) ? tax = 0 : tax;
+        return tax;
     }
 }
+
