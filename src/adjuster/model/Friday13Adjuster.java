dc268e34da4c25748dc916082852f8605a335606
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-08-15 19:23:55 +0300

    Working days

--- src/adjuster/model/Friday13Adjuster.java
+++ src/adjuster/model/Friday13Adjuster.java
@@ -3,44 +3,14 @@ package adjuster.model;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Year;
-import java.time.temporal.ChronoField;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAdjuster;
-import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.*;
 
 public class Friday13Adjuster implements TemporalAdjuster {
     @Override
     public Temporal adjustInto(Temporal temporal) {
-//        LocalDate date = LocalDate.now();
-//        date = date.withYear(2025);
-//        date = date.withMonth(3);
-//        date = date.withDayOfMonth(28);
-//        date = date.withDayOfYear(256);
-//        System.out.println(date);
-//
-//        date = date.with(ChronoField.YEAR, 2022); // какое поле на что изменить
-//        System.out.println(date);
-//
-//        date = date.with(TemporalAdjusters.firstDayOfNextMonth());
-//        System.out.println(date);
-//        date = date.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
-//        System.out.println(date);
-//        date = date.with(TemporalAdjusters.dayOfWeekInMonth(4, DayOfWeek.FRIDAY)); // 4 пятница в этом месяце
-//        System.out.println(date);
-
-//        LocalDate ls = LocalDate.from(temporal);
-//        System.out.println(ls);
-        LocalDate ld = LocalDate.from(temporal);
-        while ((ld.getDayOfWeek() == DayOfWeek.FRIDAY && ld.getDayOfMonth() == 13) != true) {
-            ld = ld.plusDays(1);
+        while ((temporal.get(ChronoField.DAY_OF_WEEK) == 5 && temporal.get(ChronoField.DAY_OF_MONTH) == 13) != true) {
+               temporal = temporal.plus(1, ChronoUnit.DAYS);
         }
-//        for loop
-//        for (int i = temporal.get(ChronoField.DAY_OF_YEAR); i < 500; i++) {
-//            ld = ld.plusDays(1);
-//            if ( ld.getDayOfWeek() == DayOfWeek.FRIDAY && ld.getDayOfMonth() == 13) {
-//                return ld;
-//            }
-//        }
-        return ld;
+        return temporal;
     }
 }
--- src/adjuster/model/WorkingDaysAdjuster.java
+++ src/adjuster/model/WorkingDaysAdjuster.java
@@ -1,22 +1,40 @@
 package adjuster.model;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
 public class WorkingDaysAdjuster implements TemporalAdjuster {
-	int nDays;
-	DayOfWeek[] daysOff;
+    int nDays;
+    DayOfWeek[] daysOff;
 
-	public WorkingDaysAdjuster(int nDays, DayOfWeek[] daysOff) {
-		this.nDays = nDays;
-		this.daysOff = daysOff;
-	}
+    public WorkingDaysAdjuster(int nDays, DayOfWeek[] daysOff) {
+        this.nDays = nDays;
+        this.daysOff = daysOff;
+    }
 
-	@Override
-	public Temporal adjustInto(Temporal temporal) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
 
+
+        LocalDate ld = LocalDate.from(temporal);
+
+        for (int i = 0; i < nDays; i++) {
+            ld = ld.plusDays(1);
+            if (ld.getDayOfWeek() == daysOff[0]) {
+                ld = ld.plusDays(2);
+            }else if (ld.getDayOfWeek() == daysOff[1]){
+                ld = ld.plusDays(2);
+            }
+
+        }
+        return ld;
+
+//        while ((temporal.get(ChronoField.DAY_OF_WEEK) == 5 && temporal.get(ChronoField.DAY_OF_MONTH) == 13) != true) {
+//            temporal = temporal.plus(1, ChronoUnit.DAYS);
+//        }
+    }
 }
