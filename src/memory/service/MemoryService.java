03f0a47c901cb9aaf2ae16193e9a419fcaf28c53
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-08-16 18:06:22 +0300

    memory

--- src/memory/service/MemoryService.java
+++ src/memory/service/MemoryService.java
@@ -4,7 +4,7 @@ public class MemoryService {
     public static int getMaxAvailableMemory() {
         int left = 0;
         int right = Integer.MAX_VALUE;
-//        int middle = 0;
+        int middle = 0;
 
         while (left <= right) {
             middle = (left + right) / 2;
@@ -20,7 +20,7 @@ public class MemoryService {
     private static boolean trueArrLength(int arrLength) {
         try {
             int[] arr = new int[arrLength];
-
+            return true;
         } catch (Throwable err) {
             return false;
         }
