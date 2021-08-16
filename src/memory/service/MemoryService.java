0e7b0a8f2f20e0675568a2cb31cb768ef6f2913f
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-08-16 18:06:03 +0300

    memory

---
+++ src/memory/service/MemoryService.java
@@ -0,0 +1,28 @@
+package memory.service;
+
+public class MemoryService {
+    public static int getMaxAvailableMemory() {
+        int left = 0;
+        int right = Integer.MAX_VALUE;
+//        int middle = 0;
+
+        while (left <= right) {
+            middle = (left + right) / 2;
+            if (!trueArrLength(middle + 1) && trueArrLength(middle)) {
+                return middle;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return middle;
+    }
+
+    private static boolean trueArrLength(int arrLength) {
+        try {
+            int[] arr = new int[arrLength];
+
+        } catch (Throwable err) {
+            return false;
+        }
+    }
+}
---
+++ src/memory/tests/MemoryServiceTest.java
@@ -0,0 +1,36 @@
+package memory.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import memory.service.MemoryService;
+
+class MemoryServiceTest {
+
+	@Test
+	void testGetMaxAvailableMemory() {
+		int[] arr;
+		int maxMemory = MemoryService.getMaxAvailableMemory();
+		boolean flag;
+
+		try {
+			arr = new int[maxMemory];
+			flag = true;
+		} catch (Throwable e) {
+			flag = false;
+		}
+		assertTrue(flag);
+
+		try {
+			arr = new int[maxMemory + 1];
+			flag = true;
+		} catch (Throwable e) {
+			flag = false;
+		}
+		assertFalse(flag);
+
+	}
+
+}
