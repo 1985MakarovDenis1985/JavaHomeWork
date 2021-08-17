ccdcd39bf5f2b9391e5e58274330f15bfcfa2ba8
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-08-17 15:01:11 +0300

    fibonacci Iterator

---
+++ src/fibonacci/controller/FibonacciApp.java
@@ -0,0 +1,20 @@
+package fibonacci.controller;
+
+import fibonacci.model.Fibonacci;
+
+/*
+ * 1, 1, 2, 3, 5, 8,13, 21
+ */
+
+public class FibonacciApp {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci(8);
+        int sum = 0;
+        for (Integer num : fibonacci) {
+            System.out.println(num);
+            sum += sum + num;
+        }
+        System.out.println("Sum= " + sum);
+
+    }
+}
---
+++ src/fibonacci/model/Fibonacci.java
@@ -0,0 +1,46 @@
+package fibonacci.model;
+
+import java.util.Iterator;
+
+public class Fibonacci implements Iterable<Integer> {
+    private int quantity;
+
+    public Fibonacci(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new FibonacciIteratorN();
+    }
+
+
+    private class FibonacciIteratorN implements Iterator<Integer> {
+        private int current = 0;
+        private int a = 0;
+        private int b = 1;
+
+        @Override
+        public boolean hasNext() {
+            return current < quantity;
+        }
+
+        @Override
+        public Integer next() {
+            current++;
+            int value = a + b;
+            a = b;
+            b = value;
+            return value;
+        }
+    }
+
+}
