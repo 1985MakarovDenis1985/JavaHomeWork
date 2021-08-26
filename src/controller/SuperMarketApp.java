6725ab294708453052d62761e4b8514417c78393
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-08-26 15:39:37 +0300

    --- supermarket with collection ---

--- src/controller/SuperMarketApp.java
+++ src/controller/SuperMarketApp.java
@@ -18,18 +18,21 @@ public class SuperMarketApp {
         supermarket.addProduct(new Product(5000, "Beer", "drink", "TacoBell", 57.0, LocalDate.of(2021, 7, 12)));
         supermarket.addProduct(new Product(6000, "Fish", "food", "McDonald’s", 23.0, LocalDate.of(2022, 3, 12)));
 
-        System.out.println("BarCode : " + supermarket.findByBarCode(3000)); // find
-        supermarket.removeProduct(3000); // remove
-        supermarket.removeProduct(7000); // null
+        System.out.println(" ------- allProducts -------");
+        for (Product p : supermarket) { System.out.println(p); }
 
-        System.out.println(" ------- findProductWithExpDat -------");
-        for (Product p : supermarket.findProductWithExpDate()) { System.out.println(p); }
-
-        System.out.println(" ------- findByCategory -------");
-        for (Product p : supermarket.findByCategory("drink")) { System.out.println(p); }
-
-        System.out.println(" ------- findByBrand -------");
-        for (Product p : supermarket.findByBrand("TacoBell")) { System.out.println(p); }
+//        System.out.println("BarCode : " + supermarket.findByBarCode(3000)); // find
+//        supermarket.removeProduct(3000); // remove
+//        supermarket.removeProduct(7000); // null
+//
+//        System.out.println(" ------- findProductWithExpDat -------");
+//        for (Product p : supermarket.findProductWithExpDate()) { System.out.println(p); }
+//
+//        System.out.println(" ------- findByCategory -------");
+//        for (Product p : supermarket.findByCategory("drink")) { System.out.println(p); }
+//
+//        System.out.println(" ------- findByBrand -------");
+//        for (Product p : supermarket.findByBrand("TacoBell")) { System.out.println(p); }
 
     }
 }
