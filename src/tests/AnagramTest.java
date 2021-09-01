6769130cb20c77d056fe76122f2f31d51007efe5
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-09-01 15:19:05 +0300

    --- Anagrams ---

---
+++ src/tests/AnagramTest.java
@@ -0,0 +1,31 @@
+//package tests;
+//
+//import model.Anagram;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class AnagramTest extends Anagram {
+//
+//    String word = "electricity";
+//
+//    @Test
+//    void testIsAnagram() {
+//        assertTrue(isAnagram(word, "city"));
+//        assertTrue(isAnagram(word, "tic"));
+//        assertTrue(isAnagram(word, "City"));
+//        assertTrue(isAnagram(word, "rele"));
+//        assertTrue(isAnagram(word, "tele"));
+//    }
+//
+//    @Test
+//    void testIsNotAnagram() {
+//        assertFalse(isAnagram(word, "tell"));
+//        assertFalse(isAnagram(word, "select"));
+//        assertFalse(isAnagram(word, "collect"));
+//        assertFalse(isAnagram(word, ""));
+//        assertFalse(isAnagram(word, null));
+//        assertFalse(isAnagram(word, "reter"));
+//    }
+//}
