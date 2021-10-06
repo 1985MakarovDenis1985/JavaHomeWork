825808ec1c064dc8ec44e89ebccaf3b7ac601437
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-10-06 20:11:21 +0300

    calc by reflection

--- src/controller/CalculatorApp.java
+++ src/controller/CalculatorApp.java
@@ -2,23 +2,16 @@ package controller;
 
 import model.Calculator;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class CalculatorApp {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchMethodException {
         if (args.length != 3){
             System.out.println("Wrong numbers of argument");
             return;
         }
 
         Calculator calculator = new Calculator();
-        Method method = Calculator.class.getDeclaredMethod(args[0], int.class, int.class);
-        method.setAccessible(true);
-
-        method.invoke(calculator, Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        calculator.calc(args[0], args[1], args[2]);
     }
 }
--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -4,43 +4,36 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Calculator {
-    //    public void  sum(int a, int b){
-//        int res = a + b;
-//        System.out.println("Sum : " + a + " + " + b + " = " + res);
-//    }
-    public void sum(int a, int b) {
+        public void  sum(int a, int b){
         int res = a + b;
-        System.out.println("Sum : " + res);
+        System.out.println("Sum : " + a + " + " + b + " = " + res);
     }
 
-//    public void sub(int a, int b) {
-//        int res = a - b;
-//        System.out.println("Sum : " + a + " - " + b + " = " + res);
-//    }
-//
-//    public void multiplication(int a, int b) {
-//        int res = a * b;
-//        System.out.println("Sum : " + a + " * " + b + " = " + res);
-//    }
-//
-//    public void div(int a, int b) {
-//        int res = a / b;
-//        System.out.println("Sum : " + a + " / " + b + " = " + res);
-//    }
+    public void sub(int a, int b) {
+        int res = a - b;
+        System.out.println("Sub : " + a + " - " + b + " = " + res);
+    }
 
-//    public void calc(String funName, int firstArg, int secondArg) throws NoSuchMethodException {
-//
-//        Class<Calculator> clazz = Calculator.class;
-//        try {
-//            Method method = clazz.getDeclaredMethod(funName, int.class, int.class);
-//            method.invoke(this, firstArg, secondArg);
-//
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public void multiplication(int a, int b) {
+        int res = a * b;
+        System.out.println("Multiplication : " + a + " * " + b + " = " + res);
+    }
+
+    public void div(int a, int b) {
+        int res = a / b;
+        System.out.println("Div : " + a + " / " + b + " = " + res);
+    }
+
+    public void calc(String funName, String firstArg, String secondArg) throws NoSuchMethodException {
+
+        Class<?> clazz = Calculator.class;
+        try {
+            Method method = clazz.getDeclaredMethod(funName, int.class, int.class);
+            method.invoke(this, Integer.parseInt(firstArg), Integer.parseInt(secondArg));
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
