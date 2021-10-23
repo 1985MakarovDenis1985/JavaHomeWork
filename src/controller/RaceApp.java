756ebae32ddc20bce20c38ae9ea1c38ad85301f9
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-10-23 18:09:35 +0300

    Cockroaches races with BufferedReader

--- src/controller/RaceApp.java
+++ src/controller/RaceApp.java
@@ -2,11 +2,25 @@ package controller;
 
 import model.Race;
 
-public class RaceApp{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args) {
+import static java.lang.System.out;
+
+public class RaceApp {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        out.println("Inter count of Taracans:");
+        String countOfTarakans = br.readLine();
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out.println("Inter dist:");
+        String dist = br.readLine();
+
         Race race = new Race();
-        race.startRace(args);
+        race.startRace(countOfTarakans, dist);
     }
 
 }
