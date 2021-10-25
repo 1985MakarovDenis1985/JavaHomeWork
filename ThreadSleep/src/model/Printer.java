5ad354019d9ef1a7e73e1e3d0912e16763caf5e1
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2021-10-25 16:42:05 +0300

    Thread Sleep

--- ThreadSleep/src/model/Printer.java
+++ ThreadSleep/src/model/Printer.java
@@ -2,8 +2,9 @@ package model;
 
 public class Printer implements Runnable {
     Thread threadLink;
-    final int TOTAL = 103;
+    final int TOTAL = 11;
     final int CHUNKS = 5;
+    int x = 0;
     int num = 0;
 
     public int getNum() {
@@ -24,28 +25,40 @@ public class Printer implements Runnable {
 
     void startPrint() {
         for (int i = 1; i < TOTAL + 1; i++) {
+
+            x++;
+            int a = TOTAL%CHUNKS;
+            int b = TOTAL-a;
             System.out.println(num);
             if (i % CHUNKS == 0) {
-                if (i == TOTAL) {
-                    System.out.println(num + "finish");
-                }
                 threadLink.interrupt();
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
 
                 }
-            }
-        }
-    }
 
+            }
+            if (i>b && i % a == 0) {
+                threadLink.interrupt();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+
+            }
+
+        }
+
+    }
 
     @Override
     public void run() {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
-            startPrint();
+                startPrint();
         }
     }
 }
